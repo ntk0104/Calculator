f1dd0852241cb825e8fb5162da590e1343fcc481
Roy Nguyen <roynguyen147@gmail.com>
2018-09-27 14:47:05 +0700

    add function multi to main

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 var kiet_lib = require('./kiet_lib');
 
 var kq = kiet_lib.add(4,5);
-console.log(kq);
+var kq_2 = kiet_lib.multi(4,5);
+
+console.log("Result of addition: " + kq);
+console.log("Result of multiply: " + kq_2);
